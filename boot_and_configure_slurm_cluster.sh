20985c9951bdc43cb584460e25bff9daade3c295
Pablo Escobar <pescobar001@gmail.com>
2020-09-16 15:44:39 +0200

    added playbooks to install Rstudio in login node

--- boot_and_configure_slurm_cluster.sh
+++ boot_and_configure_slurm_cluster.sh
@@ -4,3 +4,4 @@ ansible-playbook -e @config/slurm_cluster_switch_cloud.yml ansible/slurm_cluster
 ansible-playbook -i ansible/inventory/hosts -e @config/slurm_cluster_switch_cloud.yml ansible/slurm_cluster_configure_nfs_server.yml
 ansible-playbook -i ansible/inventory/hosts -e @config/slurm_cluster_switch_cloud.yml ansible/slurm_cluster_configure_slurm_daemons.yml
 ansible-playbook -i ansible/inventory/hosts -e @config/slurm_cluster_switch_cloud.yml ansible/slurm_cluster_configure_user_accounts.yml
+ansible-playbook -i ansible/inventory/hosts -e @config/slurm_cluster_switch_cloud.yml ansible/slurm_cluster_install_rstudio_login_node.yml
