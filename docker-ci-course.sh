46ba8e23247c7c504808fa9fdca6f444f7813785
Pablo Escobar <pablo.escobarlopez@unibas.ch>
2021-09-08 16:23:50 +0200

    added wrapper and config for docker-ci course

---
+++ docker-ci-course.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ansible-playbook -e @config/docker_ci_course_switch_cloud.yml ansible/docker-ci-course-01-boot-openstack.yml
+ansible-playbook -e @config/docker_ci_course_switch_cloud.yml -i ansible/inventory/docker_ci_course ansible/docker-ci-course-02-configure.yml
