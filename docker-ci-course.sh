2cc50ac44bf308b53745bbbf9d6b350e86e8001b
Pablo Escobar <pablo.escobarlopez@unibas.ch>
2021-11-23 09:56:05 +0100

    changes to docker-gitlab course which I don't remember :)

--- docker-ci-course.sh
+++ docker-ci-course.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ansible-playbook -e @config/docker_ci_course_switch_cloud.yml ansible/docker-ci-course-01-boot-openstack.yml
-ansible-playbook -e @config/docker_ci_course_switch_cloud.yml -i ansible/inventory/docker_ci_course ansible/docker-ci-course-02-configure.yml
+ansible-playbook -e @config/docker_ci_course_switch_cloud.yml -i ansible/inventory/docker_ci_course ansible/docker-ci-course-02-configure.yml -f 20
